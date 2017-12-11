2e21b0e5669b0066ece9f9cfc4432dbf89cfe1c4
Ivan Koretskyy <ivan.koretskyy@cgm.com>
2017-12-11 18:16:39 +0200

    update servejs

--- server.js
+++ server.js
@@ -1,17 +1,45 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+let mongoose = require('mongoose');
 const jwt = require('jwt-simple');
 let app  = express();
 const posts = [  {"message": "message1rte"},  {"message": "message2"}];
-
+mongoose.Promise = Promise;
 
 const User = require('./models/user');
 
 app.use(cors());
 app.use(bodyParser.json())
 app.get('/posts',(req,res) => {  res.send(posts)});
+
+app.get('/users', async (req,res) => {
+    try{
+
+        const users = await User.find({},'-password -__v');
+        res.status(200).send(users);
+    }
+     catch(err)
+    {
+        res.sendStatus(500);
+        console.error(err);
+    }
+
+});
+
+app.get('/profile/:id', async (req,res) => {
+    try{
+
+        const user = await User.findById(req.params.id, '-password -__v');
+        res.status(200).send(user);
+    }
+     catch(err)
+    {
+        res.sendStatus(500);
+        console.error(err);
+    }
+
+});
 
 app.post('/register',(req,res) => {
     let userDate = req.body;
