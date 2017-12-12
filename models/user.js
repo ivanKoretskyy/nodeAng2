a2ed88868fbd54a7292879f66ab4a8ea38b3c641
Ivan Koretskyy <ivankoretskyy@gmail.com>
2017-12-12 09:07:35 +0200

    add bcrypt

--- models/user.js
+++ models/user.js
@@ -1,8 +1,37 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.model('User', {
+const bcrypt = require('bcrypt');
+
+let userSchema = new mongoose.Schema({
     email: String,
     password: String,
     name: String,
     description: String
+});
+
+userSchema.pre('save', function(next) {
+    let user = this;
+
+    if(!user.isModified('password')) {
+        return next();
+    }
+    console.log('gona bcrypt')
+    bcrypt.genSalt()
+    bcrypt.genSalt(10,(err,salt) => {
+        if(err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password,10,(err,hash)=> {
+            if(err){
+                return next(err)
+            }
+            user.password =hash;
+            next();
+        }) 
+       
+    })
+   
+
 })
+
+module.exports = mongoose.model('User', userSchema );
